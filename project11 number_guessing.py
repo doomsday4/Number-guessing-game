aec188f0d6e0971ba81edac63f72d043637caa4e
doomsday4 <93817982+doomsday4@users.noreply.github.com>
2022-04-27 23:51:21 +0530

    Add files via upload

---
+++ project11 number_guessing.py
@@ -0,0 +1,27 @@
+import random
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+number = random.randint(1, 100)
+
+diff = input("Choose difficulty. Type 'easy' or 'hard': ")
+if diff == "easy":
+  attempts = 10
+elif diff == "hard":
+  attempts = 5
+else:
+  print("Invalid Input.")
+
+while attempts > 0:
+  print(f"You have {attempts} attempts remaining to guess the number.")
+  guess = int(input("Make a guess: "))
+  attempts -= 1
+  if guess == number:
+    print("You Won!")
+    break
+  elif guess > number:
+    print("Too High")
+  elif guess < number:
+    print("Too Low.")
+
+  if attempts == 0:
+    print(f"You loose. The number was {number}. Better Luck next time.")
